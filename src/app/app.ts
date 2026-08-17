d9ff8e856bb084c015daf7b26c99fe552f818fd8
Lokeshwari V <lokeshwari.tupalle@epitomecloud.com>
2026-08-17 15:48:54 +0200

    Styling changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,10 +6,11 @@ import { About } from "./features/about/about";
 import { Experience } from './features/experience/experience';
 import { Projects } from './features/projects/projects';
 import { Contact } from './features/contact/contact';
+import { Footer } from "./shared/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Nav, Home, Home, About, Experience, Projects, Contact],
+  imports: [RouterOutlet, Nav, Home, Home, About, Experience, Projects, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
